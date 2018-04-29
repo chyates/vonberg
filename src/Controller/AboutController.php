477b286a50685ff83839709926614e841f8f009f
Darren Mckeeman <darren.mckeeman@gmail.com>
2018-04-29 08:32:38 -0500

    allow index pages of controllers without login

--- src/Controller/AboutController.php
+++ src/Controller/AboutController.php
@@ -5,6 +5,11 @@ use App\Controller\AppController;
 
 class AboutController extends AppController
 {
+    public function beforeFilter(Event $event)
+    {
+        // allow all action
+        $this->Auth->allow(['index']);
+    }
     public function index()
     {
 
--- src/Controller/ResourcesController.php
+++ src/Controller/ResourcesController.php
@@ -5,6 +5,11 @@ use App\Controller\AppController;
 
 class ResourcesController extends AppController
 {
+    public function beforeFilter(Event $event)
+    {
+        // allow all action
+        $this->Auth->allow(['index']);
+    }
     public function index()
     {
 
